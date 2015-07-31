9714ad2f813a8fadc6e63921537fd391142cc97b
Korbinian Rosenegger <kroseneg@schmidham.net>
2015-07-31 14:48:06 +0200

    Add some comments

--- src/Lock.php
+++ src/Lock.php
@@ -4,6 +4,11 @@ abstract class Lock {
 	private $locks = array();
 	public $debug = !false;
 
+
+	/**
+	 * Ensure that there's only one instance of the Lock class
+	 */
+
 	static function factory($type, $data=null) {
 		require_once(dirname(__FILE__) . "/Lock/" . $type . ".php");
 		$class = "Lock_" . $type;
@@ -13,6 +18,11 @@ abstract class Lock {
 	protected function __construct() {
 	}
 
+
+	/**
+	 * Release all held locks on destruction
+	 */
+
 	public function __destruct() {
 		$this->debug(__CLASS__ . "::" . __FUNCTION__);
 		foreach (array_keys($this->locks) as $key) {
@@ -20,20 +30,42 @@ abstract class Lock {
 		}
 	}
 
+
+	/**
+	 * Lock $key
+	 */
+
 	final public function lock($key) {
 		$this->locks[$key] = true;
 		$this->do_lock($key);
 	}
+
+	/**
+	 * Unlock $key
+	 */
+
 	final public function unlock($key) {
 		$this->do_unlock($key);
 		unset($this->locks[$key]);
 	}
-	abstract protected function do_lock($key);
-	abstract protected function do_unlock($key);
+
+
+	/**
+	 * Output $string if debugging is enabled
+	 */
 
 	protected function debug($string) {
 		if ($this->debug) print $string . "\n";
 	}
+
+
+	/**
+	 * Lock drivers have to implement these functions which do the actual work
+	 */
+
+	abstract protected function do_lock($key);
+	abstract protected function do_unlock($key);
+
 }
 
 class LockException extends Exception {
