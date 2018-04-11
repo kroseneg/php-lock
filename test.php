d6843f9d01611c1baacf4f35db604ca946b6066c
Korbinian Rosenegger <kroseneg@schmidham.net>
2018-04-11 13:48:08 +0200

    Randomize keys in test.php for deadlock testing

--- test.php
+++ test.php
@@ -1,16 +1,30 @@
 <?php
-require ("Lock.php");
+require ("src/Lock.php");
 
 
+#$lock = Lock::factory("OLD");
 $lock = Lock::factory("Dreadlock");
 
 
+#$lock->debug = true;
 
-print "Locking...\n";
-$lock->lock("ABCD");
-$lock->lock("test1");
-$lock->lock("test2");
-$lock->lock("test3");
+$arr = array(
+	"bla::fasel",
+	"ABCD",
+	"test1",
+	"test2",
+	"test3",
+	"test4",
+);
+
+shuffle($arr);
+
+print "Locking... ";
+foreach ($arr as $key) {
+	print "$key ";
+	$lock->lock($key);
+	sleep(1);
+}
 print "Locked.\n";
 sleep(3);
 print "Un-Locking...\n";
